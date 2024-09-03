c634619511e110c8cec79a2fbf1bba66d557b2be
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-03 13:48:51 -0300

    fix: callback

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,26 +1,33 @@
-const dotenv = require("dotenv").config();
-
 const config = {
-  SERVER: process.env.SERVER_FRONT,
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8080,
+  PRODUCTION_URL: process.env.PRODUCTION_URL,
   MONGO_URL: process.env.MONGO_URL,
+  MONGO_URL_TEST: process.env.MONGO_URL_TEST,
   DB_NAME: process.env.DB_NAME,
-  PERSISTENCE: process.env.PERSISTENCE || "FS",
-  NODEMAILER_PASS: process.env.NODEMAILER_PASS,
-  NODEMAILER_GMAIL: process.env.NODEMAILER_GMAIL,
-  JWT_RESET_PASSWORD_KEY: process.env.JWT_RESET_PASSWORD_KEY,
   PRIVATE_KEY: process.env.PRIVATE_KEY,
+  PERSISTENCE: process.env.PERSISTENCE,
+  SECRET_JWT: process.env.SECRET_JWT,
+  SERVER_FRONT: process.env.SERVER_FRONT,
+
+  GITHUB_CALLBACK_URL: process.env.NODE_ENV === 'production'
+    ? process.env.GITHUB_CALLBACK_URL_PROD
+    : process.env.GITHUB_CALLBACK_URL,
+  GOOGLE_CALLBACK_URL: process.env.NODE_ENV === 'production'
+    ? process.env.GOOGLE_CALLBACK_URL_PROD
+    : process.env.GOOGLE_CALLBACK_URL,
+
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
-  CALLBACK_URL: process.env.CALLBACK_URL,
+
   PASS_COOKIE: process.env.PASS_COOKIE,
-  MONGO_URL_TEST: process.env.MONGO_URL_TEST,
-  PRODUCTION_URL: process.env.PRODUCTION_URL,
-  MODE: process.env.MODE || "production",
+  JWT_RESET_PASSWORD_KEY: process.env.JWT_RESET_PASSWORD_KEY,
+  NODEMAILER_GMAIL: process.env.NODEMAILER_GMAIL,
+  NODEMAILER_PASS: process.env.NODEMAILER_PASS,
+
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
 };
 
 module.exports = config;
