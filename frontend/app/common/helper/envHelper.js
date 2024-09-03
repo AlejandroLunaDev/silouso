90ccc8f17f2447726474e26bd516544a98faee54
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-03 13:14:08 -0300

    fix: isproduction frontend

--- frontend/app/common/helper/envHelper.js
+++ frontend/app/common/helper/envHelper.js
@@ -1,7 +1,8 @@
 // src/helpers/envHelper.js
 
 export const getBaseUrl = () => {
-    if (import.meta.env.PROD) {
+  const isProduction = import.meta.env.MODE === 'production';
+    if (isProduction) {
       return import.meta.env.VITE_BASE_URL_PROD;
     } else {
       return import.meta.env.VITE_BASE_URL_DEV;
