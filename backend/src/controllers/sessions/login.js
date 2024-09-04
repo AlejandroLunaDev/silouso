8823c122d8124d3c8dcaa983ef3d337e846202c4
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-04 19:10:16 -0300

    test domain coockies

--- backend/src/controllers/sessions/login.js
+++ backend/src/controllers/sessions/login.js
@@ -31,8 +31,8 @@ module.exports = async (req, res) => {
       sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       secure: process.env.NODE_ENV === 'production', 
       httpOnly: process.env.NODE_ENV === 'production', 
-      domain: process.env.NODE_ENV === 'production' ? '.silouso.shop' : undefined,
-    };
+/*       domain: process.env.NODE_ENV === 'production' ? '.silouso.shop' : undefined,
+ */    };
 
     res.cookie(config.PASS_COOKIE, token, cookieOptions);
 
