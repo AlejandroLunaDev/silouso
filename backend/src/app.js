c309f903190506e1716ff41714f25650179fa837
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-06 20:11:20 -0300

    test deploy cookie

--- backend/src/app.js
+++ backend/src/app.js
@@ -45,6 +45,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/../public')));
