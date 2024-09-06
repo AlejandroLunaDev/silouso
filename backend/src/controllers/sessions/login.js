fc8a5a0d0077fa15ed7192475b58a2629203e15d
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-06 20:17:35 -0300

    test

--- backend/src/controllers/sessions/login.js
+++ backend/src/controllers/sessions/login.js
@@ -30,7 +30,6 @@ module.exports = async (req, res) => {
     let cookieOptions = {
       sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       secure: process.env.NODE_ENV === 'production', 
-      domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : undefined,
       maxAge: 24 * 60 * 60 * 1000,
   };
 
