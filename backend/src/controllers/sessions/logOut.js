799d8bfabe39c16bca11006e614d31f385224458
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-08 02:04:22 -0300

    refactor logout for deploy

--- backend/src/controllers/sessions/logOut.js
+++ backend/src/controllers/sessions/logOut.js
@@ -1,8 +1,16 @@
 const config = require("../../config/config");
 
 module.exports = (req, res) => {
-  res.clearCookie(config.PASS_COOKIE);
-  
+  // Asegúrate de que las opciones de la cookie sean las mismas que cuando se creó
+  const cookieOptions = {
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+    secure: process.env.NODE_ENV === 'production', 
+    domain: process.env.NODE_ENV === 'production' ? '.silouso.shop' : undefined, // Asegúrate de usar el dominio personalizado
+  };
+
+  // Eliminar la cookie con las mismas opciones
+  res.clearCookie(config.PASS_COOKIE, cookieOptions);
+
   // Redirigir a diferentes URL según el entorno
   const redirectUrl = process.env.NODE_ENV === 'production' 
     ? 'https://www.silouso.shop/login' // Producción
